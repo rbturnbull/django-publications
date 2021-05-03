61a13a5b3bdd4bf4c90796e6fc8e4307cfca1bc2
rbturnbull <rob@robturnbull.com>
2021-05-03 16:14:49 +1000

    removing list from watson search

--- publications/apps.py
+++ publications/apps.py
@@ -8,8 +8,6 @@ class PublicationsConfig(AppConfig):
         try:
             from watson import search as watson_search
             from .models.publication import Publication
-            from .models.list import List
             watson_search.register(Publication)
-            watson_search.register(List)
         except:
             pass
